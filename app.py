63a283db3d8145cac7bca9222863cc6ab8780a00
Konark Gautam <rahulkonarks@gmail.com>
2022-12-06 16:11:46 +0400

    Add functionality to site

--- app.py
+++ app.py
@@ -2,12 +2,28 @@ import streamlit as st
 import functions
 
 todos = functions.get_todos()
+st.set_page_config(layout="wide")
 
-st.title("TO-DOz")
-st.subheader("Never loose track again!")
-st. write("Your todos:-")
 
-for todo in todos:
-    st.checkbox(todo)
+def add_todo():
+    todo = st.session_state["new_todo"] + '\n'
+    todos.append(todo)
+    functions.write_todos(todos)
+    st.session_state["new_todo"] = ''
 
-st.text_input(label="", placeholder="Enter todo here...")
+
+st.title("My Todo App")
+st.subheader("This is my todo app.")
+st.write("This app is to increase your productivity.")
+
+
+for index_value, todo in enumerate(todos):
+    checkbox = st.checkbox(label=todo, key=index_value)
+    if checkbox:
+        todos.pop(index_value)
+        functions.write_todos(todos)
+        del st.session_state[index_value]
+        st.experimental_rerun()
+
+st.text_input(label="", placeholder="Add new todo...",
+              on_change=add_todo, key="new_todo")
